ccd6b62202e1af666d1ea8da6a66811193b40453
Prasanna Sairam <sairamg75@gmail.com>
2019-03-02 21:04:53 +0530

    Problem 3

---
+++ src/com/hackerrank/problems/countingpairs/CountingPairs.java
@@ -0,0 +1,49 @@
+package com.hackerrank.problems.countingpairs;
+
+import java.util.Arrays;
+
+/**
+ * Created by prasanna-4077.
+ */
+public class CountingPairs {
+    public static int getPairsCount(int[] numbers, int requiredDifference) {
+
+        //Sort array using internal QuickSort
+        Arrays.sort(numbers);
+
+        //Assume a lower bound and upper bound to traverse through the array and find the difference
+        int lowerPointer = 0,
+            upperPointer = 1,
+            pairCount = 0;
+
+        while (lowerPointer < numbers.length &&
+                upperPointer < numbers.length) {
+
+            //Skip repeated elements
+            if (lowerPointer != 0 && numbers[lowerPointer] == numbers[lowerPointer-1]) {
+                lowerPointer++;
+                continue;
+            }
+
+            //Upper bound should always be a greater value than the lower bound since difference is positive from constraint
+            if (lowerPointer >= upperPointer) {
+                upperPointer = lowerPointer + 1;
+                continue;
+            }
+
+            int difference = numbers[upperPointer] - numbers[lowerPointer];
+            if (difference == requiredDifference) {
+//                System.out.println( numbers[lowerPointer] + "," + numbers[upperPointer]);
+                lowerPointer++;
+                upperPointer++;
+                pairCount++;
+            } else if (difference > requiredDifference) {
+                lowerPointer++;
+            } else {
+                upperPointer++;
+            }
+        }
+
+        return pairCount;
+    }
+}
---
+++ src/com/hackerrank/problems/countingpairs/CountingPairsRunner.java
@@ -0,0 +1,57 @@
+package com.hackerrank.problems.countingpairs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by prasanna-4077.
+ */
+public class CountingPairsRunner {
+    public static void main(String[] args) {
+        List<String> testFilePathList = Arrays.asList(
+                "src/com/hackerrank/problems/countingpairs/test/TestCase1.txt",
+                "src/com/hackerrank/problems/countingpairs/test/TestCase2.txt",
+                "src/com/hackerrank/problems/countingpairs/test/TestCase3.txt",
+                "src/com/hackerrank/problems/countingpairs/test/TestCase4.txt",
+                "src/com/hackerrank/problems/countingpairs/test/TestCase5.txt"
+        );
+
+        testFilePathList.forEach(CountingPairsRunner::getPairs);
+    }
+
+    static void getPairs( String filePath ) {
+        System.out.println("Running " + filePath);
+        List<String> numbersAsString = new ArrayList<>();
+        int difference = -1;
+
+        //Buffered reader used since there is a need to retrieve first and last lines separately
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            //Get size of array
+            String line = br.readLine();
+            int size = Integer.parseInt(line);
+
+            //read till size
+            int lineCount = 0;
+            while ((lineCount++ < size) && (line = br.readLine()) != null) {
+                numbersAsString.add(line);
+            }
+
+            //Get required difference
+            difference = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("Exception in getting file -> " + filePath);
+        }
+
+        int[] numbers = numbersAsString.stream()
+                                       .mapToInt(Integer::valueOf)
+                                       .toArray();
+
+        int pairsCount = CountingPairs.getPairsCount(numbers, difference);
+        System.out.println(pairsCount);
+    }
+}
